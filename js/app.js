14989d783db94a72cdb381474caa0f30d178f05c
ercheung3 <home@Erics-MBP.fios-router.home>
2022-02-24 16:05:42 -0500

    Added menu with animations

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 $( () => {
+    /* 0: none  1: inline   2: block    3: grid     4: flex */
+    const displayType = ["none","inline","block","grid","flex"];
+
     console.log($);
     console.log("javascript is loaded.");
+
+    $(".menu-container").on("click",function() {
+        const menu = $(".navbar");
+        showElement(menu,displayType[3],true);
+        console.log("MENU CLICK");
+    });
+
+    function showElement(element,display,animate) {
+        let animateText = "";
+        if(animate) animateText = "-animated"; 
+        if(element.hasClass(`active-${display}${animateText}`)) element.removeClass(`active-${display}${animateText}`);
+        else element.addClass(`active-${display}${animateText}`);
+    }
 });
